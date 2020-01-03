046821c144945d2852e27db9f232074d4bee7752
-- <-->
2020-01-03 07:36:58 -0500

    Added Custom Theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from '@storybook/angular';
+import { addParameters, configure } from '@storybook/angular';
+import myCustomTheme from './myCustomTheme';
+import { themes } from '@storybook/theming';
+
+addParameters({
+    options: {
+        // theme: themes.dark
+        theme: myCustomTheme
+    }
+})
 
 // automatically import all files ending in *.stories.ts
 configure(require.context('../src/stories', true, /\.stories\.ts$/), module);
