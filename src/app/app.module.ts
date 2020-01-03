f63cd54505119e8b6142a217bd5547c1e4b9d291
Lyle Balcom <balcomiv@gmail.com>
2020-01-03 06:46:26 -0500

    Added "StorySource" addon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthFormComponent } from './tutorial-components/todd-motto/content-projection/auth-form/auth-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthFormComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AuthFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
